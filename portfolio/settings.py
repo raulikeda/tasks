60ef8558f66087c6e45a6410e7c1112f0b488711
duda <mariaeduardabicalho@gmail.com>
2020-11-17 15:16:24 -0300

    host -> postgres

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
-        'HOST': 'node1',
+        'HOST': '3.238.58.221',
         'PORT': '5432',
     }
 }
