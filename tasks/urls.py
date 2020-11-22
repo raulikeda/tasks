bc3dee73a1b3cce2288cede6ade2a408e05fd412
duda <mariaeduardabicalho@gmail.com>
2020-11-22 20:35:14 -0300

    client

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.show_tasks, name='show_tasks'),
+    path('', views.create_tasks, name='create_tasks'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+from django.core import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+	return HttpResponse("Hello, world. You're at the tasks index.")
+
+
+def show_tasks(request): 
+	task= Task.objects.all()
+	# serializer_class = TaskSerializer
+	jsontask = serializers.serialize("json", task)
+	return HttpResponse(jsontask.data)
+
+from django.shortcuts import render 
+
+# Create your views here. 
+def create_tasks(request): 
+	data = JSONParser().parse(request)
+	serializer = TaskSerializer(data=data)
+
+	if serializer.is_valid():
+		serializer.save()
+		return HttpResponse(serializer.data, status=201)
+	return HttpResponse(serializer.errors, status=400)
+
+
